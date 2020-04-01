d4ec36a5f6aa0f1386ecde16bb8dc6c3be76f1a2
Feng0139 <Feng0139@hotmail.com>
2020-04-02 01:41:57 +0800

    change Status

--- run.py
+++ run.py
@@ -6,13 +6,13 @@ def read_token():
         lines = f.readlines()
         return lines[0].strip()
 
-bot = commands.Bot(command_prefix = '$', case_insensitive=True)
+bot = commands.Bot(command_prefix = '$', case_insensitive=True, owner_id='341273212656680960')
 bot.config_token = read_token()
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
-    await bot.change_presence(status=discord.Status.dnd, activity=discord.Game('Cyyou! OCR -Code: SgEAh66'))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("Cyyou! OCR -Code: SgEAh66"))
 
 @bot.command(name='hello', aliases=['hi', 'Hi', 'Hello'])
 async def _ping(ctx):
