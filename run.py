a8796a1febd5e97da4880fca5b1d1cd12475d83a
Feng0139 <Feng0139@hotmail.com>
2020-04-04 17:26:16 +0800

    updata

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import json
+import mysql.connector
+
+key = json.loads(open('key.json', encoding='utf-8').read())
 
 def read_token():
     with open("token.txt", "r") as f:
@@ -7,7 +11,8 @@ def read_token():
         return lines[0].strip()
 
 bot = commands.Bot(command_prefix = '$', case_insensitive=True, owner_id='341273212656680960')
-bot.config_token = read_token()
+bot.config_token = key["token"]
+# bot.config_token = read_token()
 
 @bot.event
 async def on_ready():
