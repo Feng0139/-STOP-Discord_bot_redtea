8a7e76213d2443ca3be2f093717e726ca24df9b4
Feng0139 <Feng0139@hotmail.com>
2020-04-05 15:36:47 +0800

    updata

--- run.py
+++ run.py
@@ -62,7 +62,7 @@ async def test(ctx, *, message=None):
         embed.add_field(name="Thread Num", value=str(buf[4]), inline=True)
         embed.add_field(name="Post Num", value=str(buf[3]), inline=True)
         
-        await client.say(embed=embed)
+        await bot.say(embed=embed)
 
     except:
         mydb = relink_mydb()
