09cf707de3262886ff16f65ec22cafdf7b85e449
Feng0139 <Feng0139@hotmail.com>
2020-07-14 13:18:11 +0800

    updata

--- run.py
+++ run.py
@@ -1,61 +1,21 @@
 import discord
 from discord.ext import commands
 import json
-import mysql.connector
-
-def read_token():
-    with open("token.txt", "r") as f:
-        lines = f.readlines()
-        return lines[0].strip()
-
-def relink_mydb():
-    return mysql.connector.connect(host=key["ipaddress"],user=key["username"],passwd=key["password"],database=key["username"])
-
-key = json.loads(open('key.json', encoding='utf-8').read())
 
 # setting prefix & online status & admin id
 bot = commands.Bot(command_prefix = '$', case_insensitive=True, owner_id='341273212656680960')
 bot.remove_command('help')
 
+key = json.loads(open('key.json', encoding='utf-8').read())
 bot.config_token = key["token"]
-# bot.config_token = read_token()
-
-mydb = relink_mydb()
-mycursor = mydb.cursor()
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Game("Command: $help"))
 
-@bot.command()
-async def displayEmbed(ctx, *, message=None):
-    """
-    Embed Test
-    """
-    embed = discord.Embed(
-        title = 'Title',
-        description = 'this is a description.',
-        colour = discord.Color.blue()
-    )
-
-    embed.set_footer(text="this is a footer.")
-    embed.set_image(url='https://www.teeworlds.cn/images/logo.png')
-    embed.set_thumbnail(url='https://www.teeworlds.cn/images/logo.png')
-    embed.set_author(name='Author Name', icon_url='https://www.teeworlds.cn/images/logo.png')
-    embed.add_field(name='Field Name', value='Field Value', inline=False)
-    embed.add_field(name='Field Name', value='Field Value', inline=False)
-    embed.add_field(name='Field Name', value='Field Value', inline=True)
-
-    await ctx.send(embed=embed)
-    
-@bot.command()
-async def help(ctx, *, message=None):
-    """
-    Help panel
-    """
-    author = ctx.message.author
-
+@bot.command(name='help')
+async def _help(ctx, *, message=None):
     embed = discord.Embed(
         title = '帮助面板',
         description = 'RedTea 将提供以下所有帮助.',
@@ -69,75 +29,24 @@ async def help(ctx, *, message=None):
     embed.add_field(name='$hello', value='Hi!')
     embed.add_field(name='$ping', value='Pong!')
     embed.add_field(name='$create_time', value='RedTea 的创建时间.', inline=False)
-    #embed.add_field(name='$searchn', value='按 用户名 查找信息.', inline=False)
-    #embed.add_field(name='$searchu', value='按 UID 查找信息.', inline=False)
 
-    #await ctx.send(embed=embed)
-    await bot.send_message(author, embed=embed)
+    await ctx.send(embed=embed)
 
-
-@bot.command(aliases=['search'])
-async def searchu(ctx, *, message=None):
-    """
-    Search in UID(num)
-    """
-    try:
-        # mydb.ping()
-
-        sql = "select uid, username, newpoints, postnum, threadnum from `tws_users` where uid = " + message + ";"
-
-        mycursor.ping(reconnect=True)
-        
-        mycursor.execute(sql)
-        buf = mycursor.fetchone()
-        await ctx.send(f'{ctx.author.mention}\n```UID: {buf[0]}\nUser: {buf[1]}\nPoints: {buf[2]}\nPost Num: {buf[3]}\nThread Num: {buf[4]}```')
-    except:
-        # mydb = relink_mydb()
-        await ctx.send(f'{ctx.author.mention} 无法查询')
-
-@bot.command()
-async def searchn(ctx, *, message=None):
-    """
-    Search in userName(str)
-    """
-    try:
-        sql = "select uid, username, newpoints, postnum, threadnum from `tws_users` where username = '" + message + "';"
-
-        mycursor.ping(reconnect=True)
-
-        mycursor.execute(sql)
-        buf = mycursor.fetchone()
-        await ctx.send(f'{ctx.author.mention}\n```UID: {buf[0]}\nUser: {buf[1]}\nPoints: {buf[2]}\nPost Num: {buf[3]}\nThread Num: {buf[4]}```')
-    except:
-        mydb = relink_mydb()
-        await ctx.send(f'{ctx.author.mention} 无法查询')
 
 @bot.command(name='hello', aliases=['hi'])
 async def _hello(ctx):
-    """
-    Hi!
-    """
     await ctx.send(f'{ctx.author.mention} Hi!')
 
 @bot.command(name='ping')
 async def _ping(ctx):
-    """
-    Pong!
-    """
-    await ctx.send(f'<@{ctx.author.id}> Pong!')
+    await ctx.send(f'<@{ctx.author.id}> Pong! {round(bot.latency * 1000)}ms')
 
 @bot.command(name='creatr_time', aliases=['c_t', 'ct'])
 async def _creatr_time(ctx):
-    """
-    Robot creation time
-    """
     await ctx.send('2020/04/01 22:55 Done!')
 
 @bot.command()
 async def echo(ctx, *, message=None):
-    """
-    Let the robot say something
-    """
     message = message or '请提供必要的文字内容.'
     await ctx.message.delete()
     await ctx.send(message)
