7c202444018379f2b080be72a2602b5cb80898df
Feng0139 <Feng0139@hotmail.com>
2020-07-14 14:13:35 +0800

    updata V2

---
+++ lib/bot/__init__.py
@@ -0,0 +1,35 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord.ext.commands import Bot as BotBase
+import json
+
+PREFIX = '$'
+OWNER_IDS = [341273212656680960]
+
+class Bot(BotBase):
+    def __init__(self):
+        self.PREFIX = PREFIX
+        self.ready = False
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
+        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+
+    def run(self, version):
+        self.VERSION = version
+        key = json.loads(open('../../data/key.json', encoding='uft-8').read())
+        self.TOKEN = key['token']
+        print('running bot...')
+        super().run(self.TOKEN, reconnect=True)
+
+    async def on_connect(self):
+        print('Bot connected')
+
+    async def on_disconnect(self):
+        print('Bot disconnected')
+
+    async def on_ready(self):
+        pass
+
+    async def on_message(self, message):
+        pass
+
+bot = Bot()
