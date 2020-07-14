a8f71c16ebfdb27d399c831355efa4673c325df8
Feng0139 <Feng0139@hotmail.com>
2020-07-14 17:08:02 +0800

    updata V2

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -20,7 +20,7 @@ class Ready(object):
         print(f' {cog} cog ready')
 
     def all_ready(self):
-        return all(getattr(self, cog) for cog in COGS)
+        return all([getattr(self, cog) for cog in COGS])
 
 class Bot(BotBase):
     def __init__(self):
