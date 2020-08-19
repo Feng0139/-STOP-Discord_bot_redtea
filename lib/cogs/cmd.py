821c8e7a08e77f7e61993ec0bfd584645d37c43b
Feng0139 <Feng0139@hotmail.com>
2020-08-20 02:33:25 +0800

    u

--- lib/cogs/cmd.py
+++ lib/cogs/cmd.py
@@ -70,7 +70,7 @@ class Cmd(Cog):
 
                     if (plNum <= 0):
                         server -= 1
-                        continue
+                        break
                     
                     plNameStr += '`'
 
@@ -115,7 +115,7 @@ class Cmd(Cog):
 
                     if (plNum <= 0):
                         server -= 1
-                        continue
+                        break
                     
                     plNameStr += '`'
 
